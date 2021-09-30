2c2cab09862ad33a1e7b77bff2d640054f87b8cf
lukecaprio <lukecaprio1@gmail.com>
2021-09-30 14:24:06 -0400

    asdlkf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Axios from "axios"
+import React, {useState} from 'react';
 
 const App = ()  =>{
+  const [joke, setJoke] = useState()
   const getJoke = () => {
-    Axios.get("https://icanhazdadjoke.com/"
+    Axios.get("https://icanhazdadjoke.com/",{
+      headers: {
+        "Accept": "application/json"
+      }
+    }  
     ).then(
       (response) => {
-        console.log(response);
+        //console.log(response);
+        setJoke(response.data.joke);
       }
     );
  };
